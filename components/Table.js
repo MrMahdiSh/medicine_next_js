129c6e4725733341efad67d4c834155a58edc7ac
MrMahdiSh <mahdishoorabi@gmail.com>
2024-06-28 16:49:03 +0330

    update

--- components/Table.js
+++ components/Table.js
@@ -15,97 +15,117 @@ const Table = ({ columns, rows, paginated = false, changePage }) => {
   ``;
 
   return (
-    <div
-      ref={tableContainerRef}
-      className="overflow-x-auto"
-      style={{ direction: "rtl" }}
-    >
-      {rows && (
-        <>
-          <table
-            className="min-w-full bg-white border border-gray-200 whitespace-nowrap"
-            style={{ direction: "ltr" }}
-          >
-            <thead>
-              <tr>
-                {[...columns].reverse().map((column, index) => (
-                  <th key={index} className="py-4 px-6 border-b text-right">
-                    {column}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {paginated === false
-                ? rows.length > 0 &&
-                  [...rows].reverse().map((row, rowIndex) => (
-                    <tr key={rowIndex}>
-                      {Object.entries(row)
-                        .reverse()
-                        .map(([key, cell], cellIndex) => (
-                          <td
-                            key={cellIndex}
-                            className="py-4 px-6 border-b text-right"
-                          >
-                            {key === "created_at" || key === "updated_at"
-                              ? formatDate(cell)
-                              : cell}
-                          </td>
-                        ))}
-                    </tr>
-                  ))
-                : rows["data"] &&
-                  rows["data"].length > 0 &&
-                  [...rows["data"]].reverse().map((row, rowIndex) => (
-                    <tr key={rowIndex}>
-                      {Object.entries(row)
-                        .reverse()
-                        .map(([key, cell], cellIndex) => (
-                          <td
-                            key={cellIndex}
-                            className="py-4 px-6 border-b text-right"
-                          >
-                            {key === "created_at" || key === "updated_at"
-                              ? formatDate(cell)
-                              : cell}
-                          </td>
-                        ))}
-                    </tr>
+    <div className="p-10 bg-white rounded-[80px]">
+      <div
+        ref={tableContainerRef}
+        className="overflow-x-auto"
+        style={{
+          direction: "rtl",
+          border: "none",
+          overflow: "hidden",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {rows && (
+          <>
+            <table
+              className="min-w-full bg-white border border-gray-200 whitespace-nowrap"
+              style={{ direction: "ltr", padding: "0 16px", border: "none" }}
+            >
+              <thead>
+                <tr>
+                  {[...columns].reverse().map((column, index) => (
+                    <th
+                      key={index}
+                      className="py-4 px-6 border-b border-black text-center"
+                    >
+                      {column}
+                    </th>
                   ))}
-            </tbody>
-          </table>
-          {paginated && rows["data"] && (
-            <div className="flex justify-center mt-4">
-              <button
-                onClick={() => {
-                  changePage(1);
-                }}
-                disabled={rows["total"] === rows["current_page"]}
-                className={`px-4 py-2 mx-1 border rounded ${
-                  rows["total"] !== rows["current_page"] ? "bg-blue-400" : ""
-                }`}
-              >
-                بعدی
-              </button>
+                </tr>
+              </thead>
+              <tbody>
+                {paginated === false
+                  ? rows.length > 0 &&
+                    [...rows].reverse().map((row, rowIndex) => (
+                      <tr key={rowIndex}>
+                        {Object.entries(row)
+                          .reverse()
+                          .map(([key, cell], cellIndex) => (
+                            <td
+                              key={cellIndex}
+                              className={`py-4 px-6 ${
+                                rowIndex === rows.length - 1
+                                  ? ""
+                                  : "border-b border-black"
+                              } text-center`}
+                            >
+                              {key === "created_at" || key === "updated_at"
+                                ? formatDate(cell)
+                                : cell}
+                            </td>
+                          ))}
+                      </tr>
+                    ))
+                  : rows["data"] &&
+                    rows["data"].length > 0 &&
+                    [...rows["data"]].reverse().map((row, rowIndex) => (
+                      <tr key={rowIndex}>
+                        {Object.entries(row)
+                          .reverse()
+                          .map(([key, cell], cellIndex) => (
+                            <td
+                              key={cellIndex}
+                              className={`py-4 px-6 ${
+                                rowIndex === rows["data"].length - 1
+                                  ? ""
+                                  : "border-b"
+                              } text-center`}
+                            >
+                              {key === "created_at" || key === "updated_at"
+                                ? formatDate(cell)
+                                : cell}
+                            </td>
+                          ))}
+                      </tr>
+                    ))}
+              </tbody>
+            </table>
+            {paginated && rows["data"] && (
+              <div className="flex justify-center mt-4">
+                <button
+                  onClick={() => {
+                    changePage(1);
+                  }}
+                  disabled={rows["total"] === rows["current_page"]}
+                  className={`px-4 py-2 mx-1 border rounded ${
+                    rows["total"] !== rows["current_page"] ? "bg-blue-400" : ""
+                  }`}
+                >
+                  بعدی
+                </button>
 
-              <span className="px-4 py-2 mx-1">
-                صفحه {rows["current_page"]} از {rows["total"]}
-              </span>
-              <button
-                onClick={() => {
-                  changePage(-1);
-                }}
-                disabled={rows["current_page"] === 1}
-                className={`px-4 py-2 mx-1 border rounded ${
-                  rows["current_page"] === 1 ? "" : "bg-blue-400"
-                }`}
-              >
-                قبلی
-              </button>
-            </div>
-          )}
-        </>
-      )}
+                <span className="px-4 py-2 mx-1">
+                  صفحه {rows["current_page"]} از {rows["total"]}
+                </span>
+                <button
+                  onClick={() => {
+                    changePage(-1);
+                  }}
+                  disabled={rows["current_page"] === 1}
+                  className={`px-4 py-2 mx-1 border rounded ${
+                    rows["current_page"] === 1 ? "" : "bg-blue-400"
+                  }`}
+                >
+                  قبلی
+                </button>
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
