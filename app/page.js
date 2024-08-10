48428b2ed12372efb7503c7eee14833947cd2863
MrMahdiSh <mahdishoorabi@gmail.com>
2024-08-10 13:25:38 +0330

    check meli code

--- app/page.js
+++ app/page.js
@@ -167,10 +167,10 @@ function Content() {
       return;
     }
 
-    // if (!checkCodeMeli(meliCode)) {
-    //   toast.error("کدملی وارد شده صحیح نمیباشد");
-    //   return;
-    // }
+    if (!checkCodeMeli(meliCode)) {
+      toast.error("کدملی وارد شده صحیح نمیباشد");
+      return;
+    }
 
     try {
       const code = await fetchData("Auth/register_send_code", "POST", {
@@ -191,10 +191,10 @@ function Content() {
   async function loginMeliCodeInputHandle() {
     setIsRegister(false);
 
-    // if (!checkCodeMeli(meliCode)) {
-    //   toast.error("کدملی وارد شده صحیح نمیباشد");
-    //   return;
-    // }
+    if (!checkCodeMeli(meliCode)) {
+      toast.error("کدملی وارد شده صحیح نمیباشد");
+      return;
+    }
 
     setIsLoading(true);
     try {
